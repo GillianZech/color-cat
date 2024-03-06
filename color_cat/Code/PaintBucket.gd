2a56b7efef0fb5ed838908ca91ff9a078607467a
Gillian Zech <ginger01@email.unc.edu>
2024-03-06 15:49:55 -0500

    May have fixed the missing inputs issue

--- color_cat/Code/PaintBucket.gd
+++ color_cat/Code/PaintBucket.gd
@@ -4,14 +4,17 @@ extends Area2D
 @onready var BUCKET_SPRITE = $BucketSprite
 
 func _physics_process(_delta):
-	_animate()
-	
-func _animate():
-	BUCKET_SPRITE.play("idle")
+	#BUCKET_SPRITE.play("idle")
+	pass
 
 func _on_body_entered(body):
 	if body.PAINT_COLOR != COLOR:
 		$BucketSplashAudio.play()
-		#BUCKET_SPRITE.stop()
 		BUCKET_SPRITE.play("splash")
 		body._change_color(COLOR)
+
+
+
+func _on_bucket_sprite_animation_finished():
+	if BUCKET_SPRITE.animation == "splash":
+		BUCKET_SPRITE.play("idle")
