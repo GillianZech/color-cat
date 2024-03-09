de7188f6e1824b6bd7ea952a5e050856441e4234
Gillian Zech <ginger01@email.unc.edu>
2024-03-09 17:20:40 -0500

    Made spikes

---
+++ color_cat/Code/Spike.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+@export var THIS_LEVEL: String = "Level1"
+
+func _on_body_entered(body):
+	get_parent().get_parent().get_parent()._update_scene(THIS_LEVEL)
+	body.FOOD_APPEARED = false
+	body.DOORS_LOCKED = true
+	get_node("Death").play()
+	body._reset()
